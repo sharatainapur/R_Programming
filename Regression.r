710757a25fa414a517cc73156c44872b9fa32872
SharatAinapur <sharatainapur@gmail.com>
2021-09-23 23:55:34 +0100

    Linear Regression 23Sep

---
+++ Regression.r
@@ -0,0 +1,13 @@
+install.packages("datarium")
+library(datarium)
+data("marketing", package = "datarium")
+head(marketing)
+var(marketing$youtube)
+var(marketing$facebook)
+var(marketing$newspaper)
+var(marketing$sales)
+
+
+
+
+
