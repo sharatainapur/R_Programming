8e25080e27bc5d5296ee07bd30cf8da7f6597b70
SharatAinapur <sharatainapur@gmail.com>
2021-09-24 00:00:52 +0100

    test2

---
+++ .ipynb_checkpoints/Basics-checkpoint.r
@@ -0,0 +1,92 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "e1707972",
+   "metadata": {},
+   "source": [
+    "# Variable Assignment in R programming"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "1198affa",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/html": [
+       "20"
+      ],
+      "text/latex": [
+       "20"
+      ],
+      "text/markdown": [
+       "20"
+      ],
+      "text/plain": [
+       "[1] 20"
+      ]
+     },
+     "metadata": {},
+     "output_type": "display_data"
+    },
+    {
+     "data": {
+      "text/html": [
+       "<ol class=list-inline>\n",
+       "\t<li>'area'</li>\n",
+       "\t<li>'breadth'</li>\n",
+       "\t<li>'length'</li>\n",
+       "</ol>\n"
+      ],
+      "text/latex": [
+       "\\begin{enumerate*}\n",
+       "\\item 'area'\n",
+       "\\item 'breadth'\n",
+       "\\item 'length'\n",
+       "\\end{enumerate*}\n"
+      ],
+      "text/markdown": [
+       "1. 'area'\n",
+       "2. 'breadth'\n",
+       "3. 'length'\n",
+       "\n",
+       "\n"
+      ],
+      "text/plain": [
+       "[1] \"area\"    \"breadth\" \"length\" "
+      ]
+     },
+     "metadata": {},
+     "output_type": "display_data"
+    }
+   ],
+   "source": [
+    "length <- 5\n",
+    "breadth <- 4\n",
+    "area = length*breadth\n",
+    "area\n",
+    "ls()\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "3.6.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
