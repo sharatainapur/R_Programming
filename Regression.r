3944ab3ef7b01d206895d9233ceb94dcddc38765
SharatAinapur <sharatainapur@gmail.com>
2021-09-24 09:57:44 +0100

    Test commit

--- Regression.r
+++ Regression.r
@@ -6,8 +6,14 @@ var(marketing$youtube)
 var(marketing$facebook)
 var(marketing$newspaper)
 var(marketing$sales)
+plot(marketing)
+model <- lm(sales ~ youtube+facebook, data=marketing)
+summary(model)
 
 
+model1 <- lm(sales ~ youtube, data=marketing)
+summary(model1)
 
-
+model2 <- lm(sales ~ facebook, data=marketing)
+summary(model2)
 
