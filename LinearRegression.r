1ba945cf52381a4a7b7aa0d74ac53efa5b5e8895
SharatAinapur <sharatainapur@gmail.com>
2021-09-28 22:06:29 +0100

    Added Code to draw Scatter Plot and abline for the fitted model

--- LinearRegression.r
+++ LinearRegression.r
@@ -1,21 +1,36 @@
 # Linear Regression Model to predict the Fuel Efficiency of Cars based considering the Car Weight
-
 dataset = mtcars
+attach(dataset)
+
+?mtcars
+
+#Plotting a Scatter plot to check the nature of relationship between variables
+plot(wt,mpg,xlab = "Weight of the Car (1000lbs)", ylab = "Fuel Efficiency of the Car (Miles/US gallon)", main = "Scatter Plot of the Weight and Fuel Efficiency of the Cars",pch=20)
 
 # Splitting the dataset into the Training set and Test set
 install.packages('caTools')
 library(caTools)
-split = sample.split(dataset$mpg, SplitRatio = 0.7)
+split = sample.split(dataset$mpg, SplitRatio = 0.9)
 trainingset = subset(dataset, split == TRUE)
 testset = subset(dataset, split == FALSE)
 
+plot(trainingset$wt, trainingset$mpg,pch=20)
 # Fitting Simple Linear Regression to the Training set
 lm.r= lm(formula = mpg ~ wt,data = trainingset)
 summary(lm.r)
-
+lm.r
 #Check the model coefficients:
 coef(lm.r)
 
+
+#Scatter Plot with the fitted line on the Training Dataset
+plot(trainingset$wt, trainingset$mpg,pch=20)
+abline(lm.r)
+
+plot(testset$wt, testset$mpg,pch=20)
+abline(lm.r)
+
+?abline
 #y=c+mx -> Linear Model ===> mpg = 38.847419 - 5.750522*(wt) 
 
 # Predicting the Test set results
@@ -36,4 +51,4 @@ ggplot() + geom_point(aes(x = testset$wt, y = testset$mpg), colour = 'red') +
   ggtitle('MPG vs WT (Test set)') +
   xlab('WT') + ylab('MPG')
 
-predict(lm.r, data.frame(wt=3.5))
+predict(lm.r, data.frame(wt=5))
