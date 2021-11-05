cb45b130b44ab27a895afb27473300720cbf2a9e
SharatAinapur <sharatainapur@gmail.com>
2021-11-05 23:14:39 +0000

    CLT new2

--- Central Limit Theorem.r
+++ Central Limit Theorem.r
@@ -4,7 +4,9 @@
 # loess - for more than one variable 
 # h too large --- oversmoothing ; if too small - undersmoothing
 
-#Central Limit Theorem
+#Central Limit Theorem - the histogram of mean indicates that the means are normally distributed.
+#Here we have made 10000 samples of UNIFORM distribution, each of size 10 with values between 0 and 1.
+
 meann=c()
 y<-c(1:10000)
 for (s in y) {
